de206766998baf44c780589f720a4e8b97a8bbe9
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-25 01:18:55 +0200

    trying out fix to cors issue

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const route  = require('./routes/routes')
 const cors = require('cors')
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    var origin = context.Request.Headers["origin"];
+    res.header('Access-Control-Allow-Origin', 'origin');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Credentials", "true");
+
 
     next();
 }
