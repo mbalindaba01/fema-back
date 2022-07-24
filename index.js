41f9ed5ec096978bdc6a104ee95223ef1e7a1315
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-25 01:09:22 +0200

    added statement to allow cross domain

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const route  = require('./routes/routes')
 const cors = require('cors')
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
 
 const app = express()
 
 app.use(express.json())
+app.use(allowCrossDomain)
 app.use(cors())
 
 app.use(route)
