7486dc643eb609d9ec6d6ed26f2ab83144becfa4
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-25 01:34:16 +0200

    added origin to cors

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-const express = require('express')()
+const express = require('express')
 const route  = require('./routes/routes')
 const cors = require('cors')
-
-var allowCrossDomain = function(req, res, next) {
-    var origin = context.Request.Headers["origin"];
-    res.header('Access-Control-Allow-Origin', 'origin');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header("Access-Control-Allow-Credentials", "true");
-
-
-    next();
-}
 
 
 
 const app = express()
 
 app.use(express.json())
-app.use(allowCrossDomain)
-app.use(cors())
+app.use(cors({
+    origin: 'http://127.0.0.1:5173'
+}))
 
 app.use(route)
 const PORT = process.env.PORT || 5000
