79b746b8136abe0a6f4bec05d0e43e73c04817ce
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-25 02:00:47 +0200

    cors error fix

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use((res, req, next) => {
+    res.header("Access-Control-Allow-Origin", "*"),
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept*")
+    next()
+})
 
 dotenv.config()
 
