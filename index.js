5804c69c64ac64cb5d24b12186e2f8e7e86bf287
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-07-25 17:54:37 +0200

    fix cors

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const config = {
 }
 const db = pgp(config)
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
 app.get('/', (req, res) => {
     res.json('Welcome to fema api')
 })
